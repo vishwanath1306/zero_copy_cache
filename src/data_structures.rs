19681e8124cfa25dc6dfa5d5d42fcdc03c1bc700
vishwanath1306 <vishwa.nath@outlook.com>
2023-01-20 10:38:32 -0700

    Added struct to segment

--- src/data_structures.rs
+++ src/data_structures.rs
@@ -1,3 +1,6 @@
+pub(crate) const DEFAULT_CACHE_SIZE: usize = 10_000;
+// TODO: Convert all the page sizes, and stuff to an enum with constants. 
+
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 pub struct CacheKey {
     pub key: i64,
@@ -9,16 +12,36 @@ impl CacheKey {
     }
 }
 
+#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 pub struct Stats{
     pub access_count: i64,
 }
 
+impl Stats{
+    pub fn new() -> Self{
+        Stats { access_count: 0 }
+    }
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 pub struct Segment{
     pub segment_id: i64,
     pub segment_stats: Stats,
     pub page_size: i64,
 }
 
+
+impl Segment {
+
+    pub fn new(seg_id: i64, page_size: i64) -> Self {
+
+        Segment { segment_id: seg_id, 
+                segment_stats: Stats::new(), 
+                page_size: page_size 
+        }
+    }
+    
+}
 pub struct HotSet{
     // TODO Experiment this with a hyperloglog to reduce space usage;
     pub current_hotset: Vec<Segment>
@@ -26,16 +49,16 @@ pub struct HotSet{
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 pub struct CacheValue {
-    pub value: i64,
+    pub value: Segment,
 }
 
 impl CacheValue {
     pub fn new(entry: i64) -> Self {
-        CacheValue { value: entry }
+        CacheValue { value: Segment::new(0, 2048) }
     }
 }
 
-pub(crate) const DEFAULT_CACHE_SIZE: usize = 10_000;
+
 
 pub(crate) trait CacheBuilder{
     fn put(&self, key: CacheKey, value: CacheValue) -> Option<(CacheKey, CacheValue)>;
