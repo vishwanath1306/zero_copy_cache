6cd15547620957e66b4f19286a7ba5d8bc3a00db
vishwanath1306 <vishwa.nath@outlook.com>
2023-01-23 13:34:40 -0700

    begin rewrite

--- src/data_structures.rs
+++ src/data_structures.rs
@@ -1,20 +1,10 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, hash::Hash};
 
 pub const DEFAULT_CACHE_SIZE: usize = 10_000;
 // TODO: Convert all the page sizes, and stuff to an enum with constants. 
 
-#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
-pub struct CacheKey {
-    pub key: i64,
-}
-
-impl CacheKey {
-    pub fn new(entry: i64) -> Self {
-        CacheKey { key: entry }
-    }
-}
-
-pub type SegmentStatMap = HashMap<Segment, Stats>;
+pub type SegmentStatMap< S >  = HashMap<S, Stats>;
+pub type SegmentId = i64;
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 pub struct Stats{
@@ -27,45 +17,27 @@ impl Stats{
     }
 }
 
-#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
-pub struct Segment{
-    pub segment_id: i64,
-    pub page_size: i64,
+pub trait Segment {
+
+    fn get_segment_id(&self) -> i64;
+    fn get_page_size(&self) -> u64;
+
+}
+ 
+pub struct ZeroCopyCache<S, C> where
+S: Hash + PartialEq + Eq + Clone + Segment,
+C: CacheBuilder<S>
+ {
+    pub segment_stats: SegmentStatMap<S>, 
+    pub current_pinned_list: Vec<SegmentId>,
+    pub cache_builder: C
 }
 
+pub trait CacheBuilder<S: Hash + PartialEq + Clone + Eq + Segment>{
 
-impl Segment {
-
-    pub fn new(seg_id: i64, page_size: i64) -> Self {
-
-        Segment { segment_id: seg_id, 
-                page_size: page_size 
-        }
-    }
-    
-}
-pub struct HotSet{
-    // TODO Experiment this with a hyperloglog to reduce space usage;
-    pub current_hotset: Vec<Segment>
-}
-
-#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
-pub struct CacheValue {
-    pub value: Segment,
-}
-
-impl CacheValue {
-    pub fn new(entry: i64) -> Self {
-        CacheValue { value: Segment::new(0, 2048) }
-    }
-}
-
-
-
-pub(crate) trait CacheBuilder{
-    fn put(&self, key: CacheKey, value: CacheValue) -> Option<(CacheKey, CacheValue)>;
+    fn insert(&mut self, segment: &S);
+    fn get_curr_pinned_list(&self) -> Vec<SegmentId>;
     fn get_cache_size(&self) -> usize;
-    fn get(&self, key: CacheKey) -> Option<CacheValue>;
-    fn get_hit_rate(&self) -> f64;
     fn resize_cache(&mut self, new_size: usize);
+
 }
--- src/zerocopylru.rs
+++ src/zerocopylru.rs
@@ -1,83 +1,84 @@
-use crate::data_structures::{CacheKey, CacheValue};
-use lru::LruCache;
-use std::sync::Mutex;
-use crate::data_structures::CacheBuilder;
+// use crate::data_structures::{CacheKey, CacheValue};
+// use lru::LruCache;
+// use std::{sync::Mutex, hash::Hash, str::pattern::Pattern};
+// use crate::data_structures::CacheBuilder;
 
-pub struct UnboundedLRUCache {
-    len: usize,
-    cache: Mutex<LruCache<CacheKey, CacheValue>>,
-    hit_count: u64,
-    total_count: u64,
-    miss_count: u64,
-}
+// pub struct UnboundedLRUCache<T>
+// where T: Clone + Hash + PartialEq +  {
+//     len: usize,
+//     cache: Mutex<LruCache<CacheKey, CacheValue>>,
+//     hit_count: u64,
+//     total_count: u64,
+//     miss_count: u64,
+// }
 
-impl Default for UnboundedLRUCache {
-    fn default() -> Self {
-        UnboundedLRUCache {
-            len: crate::data_structures::DEFAULT_CACHE_SIZE,
-            cache: Mutex::new(LruCache::unbounded()),
-            hit_count: 0,
-            miss_count: 0,
-            total_count: 0,
-        }
-    }
-}
+// impl Default for UnboundedLRUCache {
+//     fn default() -> Self {
+//         UnboundedLRUCache {
+//             len: crate::data_structures::DEFAULT_CACHE_SIZE,
+//             cache: Mutex::new(LruCache::unbounded()),
+//             hit_count: 0,
+//             miss_count: 0,
+//             total_count: 0,
+//         }
+//     }
+// }
 
-impl UnboundedLRUCache {
-    pub fn new(size: usize) -> UnboundedLRUCache {
-        UnboundedLRUCache {
-            len: size,
-            cache: Mutex::new(LruCache::unbounded()),
-            hit_count: 0,
-            miss_count: 0,
-            total_count: 0,
-        }
-    }
+// impl UnboundedLRUCache {
+//     pub fn new(size: usize) -> UnboundedLRUCache {
+//         UnboundedLRUCache {
+//             len: size,
+//             cache: Mutex::new(LruCache::unbounded()),
+//             hit_count: 0,
+//             miss_count: 0,
+//             total_count: 0,
+//         }
+//     }
 
-}
+// }
 
-impl CacheBuilder for UnboundedLRUCache{
+// impl CacheBuilder for UnboundedLRUCache{
 
-    fn put(&self, key: CacheKey, value: CacheValue) -> Option<(CacheKey, CacheValue)> {
-        let mut unlocked_cache = self.cache.lock().unwrap();
+//     fn put(&self, key: CacheKey, value: CacheValue) -> Option<(CacheKey, CacheValue)> {
+//         let mut unlocked_cache = self.cache.lock().unwrap();
         
-        if self.len > unlocked_cache.len() {
-            unlocked_cache.put(key, value);
-            Some((key, value))
-        } else {
-            let dropped_buffer = unlocked_cache.pop_lru();
-            unlocked_cache.put(key, value);
-            dropped_buffer
-        }
-    }
+//         if self.len > unlocked_cache.len() {
+//             unlocked_cache.put(key, value);
+//             Some((key, value))
+//         } else {
+//             let dropped_buffer = unlocked_cache.pop_lru();
+//             unlocked_cache.put(key, value);
+//             dropped_buffer
+//         }
+//     }
 
-    fn get(&self, key: CacheKey) -> Option<CacheValue> {
-        let mut unlocked_cache = self.cache.lock().unwrap();
-        let return_value = unlocked_cache.get(&key);
-        return_value.copied()
-    }
+//     fn get(&self, key: CacheKey) -> Option<CacheValue> {
+//         let mut unlocked_cache = self.cache.lock().unwrap();
+//         let return_value = unlocked_cache.get(&key);
+//         return_value.copied()
+//     }
 
-    fn get_cache_size(&self) -> usize {
-        self.cache.lock().unwrap().len()
-    }
+//     fn get_cache_size(&self) -> usize {
+//         self.cache.lock().unwrap().len()
+//     }
 
-    fn get_hit_rate(&self) -> f64 {
-        todo!()
-    }
+//     fn get_hit_rate(&self) -> f64 {
+//         todo!()
+//     }
 
-    fn resize_cache(&mut self, new_size: usize) {
-        if new_size >= self.len{
-            self.len = new_size;
-        }else{
-            let mut unlocked_cache = self.cache.lock().unwrap();
-            let difference = self.len - new_size;
-            for _ in 0..difference{
-                unlocked_cache.pop_lru();
-            }
-            self.len = new_size;
-        }
-    }
+//     fn resize_cache(&mut self, new_size: usize) {
+//         if new_size >= self.len{
+//             self.len = new_size;
+//         }else{
+//             let mut unlocked_cache = self.cache.lock().unwrap();
+//             let difference = self.len - new_size;
+//             for _ in 0..difference{
+//                 unlocked_cache.pop_lru();
+//             }
+//             self.len = new_size;
+//         }
+//     }
 
-}
+// }
 
 
