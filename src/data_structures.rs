e6dbeecadb2dd9cda885e9229fec1ff42bad9eb8
vishwanath1306 <vishwa.nath@outlook.com>
2023-01-23 18:56:14 -0700

    Added stats increment

--- src/data_structures.rs
+++ src/data_structures.rs
@@ -13,7 +13,11 @@ pub struct Stats{
 
 impl Stats{
     pub fn new() -> Self{
-        Stats { access_count: 0 }
+        Stats { access_count: 1 }
+    }
+
+    pub fn increment(&mut self){
+        self.access_count +=1;
     }
 }
 
@@ -24,20 +28,46 @@ pub trait Segment {
 
 }
  
-pub struct ZeroCopyCache<S, C> where
-S: Hash + PartialEq + Eq + Clone + Segment,
-C: CacheBuilder<S>
+pub struct ZeroCopyCache<S> where
+S: Hash + PartialEq + Eq + Clone + Segment + Default,
+// C: CacheBuilder<S>
  {
     pub segment_stats: SegmentStatMap<S>, 
     pub current_pinned_list: Vec<SegmentId>,
-    pub cache_builder: C
+    // pub cache_builder: C
 }
 
-pub trait CacheBuilder<S: Hash + PartialEq + Clone + Eq + Segment>{
+impl<S> ZeroCopyCache<S>
+where
+S: Hash + PartialEq + Eq + Clone + Segment + Default,
+// C: CacheBuilder<S>
+{
 
-    fn insert(&mut self, segment: &S);
-    fn get_curr_pinned_list(&self) -> Vec<SegmentId>;
-    fn get_cache_size(&self) -> usize;
-    fn resize_cache(&mut self, new_size: usize);
+    pub fn new(&self) -> Self{
+        ZeroCopyCache { 
+            segment_stats: SegmentStatMap::default(), 
+            current_pinned_list: Vec::new(), 
+            // cache_builder: cache_builder 
+        }
+    }
 
+    pub fn increment_count(&mut self, segment: &S){
+
+        if self.segment_stats.contains_key(segment) {
+            self.segment_stats.get_mut(segment).unwrap().increment();
+        } else {
+            // Stats constructor should automatically increment to 1
+            self.segment_stats.insert(segment.clone(), Stats::new());
+        }
+        
+    }
 }
+
+// pub trait CacheBuilder<S: Hash + PartialEq + Clone + Eq + Segment>{
+
+//     fn insert(&mut self, segment: &S);
+//     fn get_curr_pinned_list(&self) -> Vec<SegmentId>;
+//     fn get_cache_size(&self) -> usize;
+//     fn resize_cache(&mut self, new_size: usize);
+
+// }
